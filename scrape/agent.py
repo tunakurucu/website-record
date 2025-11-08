6b3558eccd82754a71850043917f3666d46bca07
TobennaU <tugwuanyi512@gmail.com>
2025-11-07 20:18:33 -0500

    agent age

--- scrape/agent.py
+++ scrape/agent.py
@@ -1,22 +1,5 @@
 from google import genai
 from dotenv import load_dotenv
-<<<<<<< HEAD
-import google.generativeai as genai
-
-load_dotenv()
-
-# Set the API key
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-# Generate content
-response = genai.generateText(
-    model="gemini-2.5-flash",
-    prompt="Tell me a fun fact about space."
-)
-
-print(response.text)
-
-=======
 import os
 
 load_dotenv()
@@ -28,4 +11,3 @@ response = client.models.generate_content(
 )
 
 print(response.text)
->>>>>>> refs/remotes/origin/main
