578d0504ec7a6e25e38442c0f56190431b22faa1
Genesis Bookard <wearebookards@gmail.com>
2025-11-07 18:56:48 -0600

    Save progress before pulling changes

--- peoples-power/src/components/Header.tsx
+++ peoples-power/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import logo from "../../../img folder/p.png";
 
 
 export default function Header() {
@@ -6,9 +7,13 @@ const { search } = useLocation();
 return (
 <header className="border-b bg-white">
 <div className="container flex h-14 items-center justify-between">
-<Link to="/" className="font-semibold">People's Power</Link>
-<nav className="text-sm text-gray-600">
-<Link to={`/${search}`} className="hover:underline">Home</Link>
+<Link to="/">
+  <img src={logo} alt="People's Power Logo" className="h-10 w-auto" />
+</Link>
+<nav className="text-sm text-gray-600 flex gap-4">
+  <Link to={`/${search}`} className="hover:underline">Home</Link>
+  <Link to="/contact" className="hover:underline">Contact</Link>
+  <Link to="/topics" className="hover:underline">Topics</Link>
 </nav>
 </div>
 </header>
